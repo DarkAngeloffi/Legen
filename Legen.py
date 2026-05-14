aca7c03dc593c6dd3fede8fa5ab89a70a28106eb
DarkAngeloffi <monasteriotesspyros9@gmail.com>
2026-05-15 00:56:07 +0300

    Update Legen.py

--- Legen.py
+++ Legen.py
@@ -31,7 +31,7 @@ STAFF_ROLE_NAME = "Support"
 # -----------------------------
 # BUTTON VIEW
 # -----------------------------
-class TicketButton(View):
+class TicketButton:
     def __init__(self):
         super().__init__(timeout=None)
 
